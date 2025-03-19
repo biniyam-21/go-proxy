10cad4fddc53e3af77532d5fa724a6935f93900e
Biniyam <biniyamxyz@gmail.com>
2025-03-19 03:31:03 +0300

    go peroxy

---
+++ part1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"net/http"
+)
+
+func main() {
+	// 1. Listen for connections forever.
+	if ln, err := net.Listen("tcp", ":8080"); err == nil {
+		for {
+			// 2. Accept connections.
+
+			// ...more code...
+			if conn, err := ln.Accept(); err == nil {
+				reader := bufio.NewReader(conn)
+				// 3. Read requests from the client.
+				if req, err := http.ReadRequest(reader); err == nil {
+					if be, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
+						be_reader := bufio.NewReader(be)
+						if err := req.Write(be); err == nil {
+							// 6. Read the response from the backend.
+
+							// ... doing something ...
+							if resp, err := http.ReadResponse(be_reader, req); err == nil {
+								// 7. Send the response to the client.
+								resp.Close = true
+								if err := resp.Write(conn); err == nil {
+									log.Printf("%s: %d", req.URL.Path, resp.StatusCode)
+								}
+								conn.Close()
+								// Repeat back at 2: accept the next connection.
+							}
+
+						}
+
+					}
+
+				}
+			}
+
+		}
+	}
+}
